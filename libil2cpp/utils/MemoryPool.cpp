NewbieGameCoder/il2cpp_plus

[opt] MemoryPool内存分配时初始化为0，Calloc时不再初始化

libil2cpp/utils/MemoryPool.cpp
@@ -68,14 +68,14 @@ namespace utils
     void* MemoryPool::Calloc(size_t count, size_t size)
     {
         void* ret = Malloc(count * size);
-        return memset(ret, 0, count * size);
+        return ret;
     }
 
     MemoryPool::Region* MemoryPool::AddRegion(size_t size)
     {
         Region* region = (Region*)IL2CPP_MALLOC(sizeof(Region));
         size_t allocationSize = std::max(kDefaultRegionSize, MakeMultipleOf(size, kPageSize));
-        region->start = region->current = (char*)IL2CPP_MALLOC(allocationSize);
+        region->start = region->current = (char*)IL2CPP_MALLOC_ZERO(allocationSize);
         region->size = region->free = allocationSize;
         m_Regions.push_back(region);
 

GameFrameX/hybridclr

libil2cpp/utils/MemoryPool.cpp
@@ -68,7 +68,7 @@ namespace utils
     void* MemoryPool::Calloc(size_t count, size_t size)
     {
         void* ret = Malloc(count * size);
-        return memset(ret, 0, count * size);
+        return ret;
     }
 
     bool MemoryPool::Contains(const void* data) const
@@ -102,7 +102,7 @@ namespace utils
             m_Regions.push_back(newRegion);
         }
 
-        newRegion->start = newRegion->current = (char*)IL2CPP_MALLOC(allocationSize);
+        newRegion->start = newRegion->current = (char*)IL2CPP_MALLOC_ZERO(allocationSize);
         newRegion->size = newRegion->free = allocationSize;
 
         return newRegion;